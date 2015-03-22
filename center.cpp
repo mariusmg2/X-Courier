1b57da36ba60bb3408d0ff0f93a9648d4fe917c5
Marius Marusanici <marius.marusanici@gmail.com>
2015-03-22 09:34:50 +0200

    rm useless comments

--- center.cpp
+++ center.cpp
@@ -4,12 +4,10 @@ Route Center::getRoute() const {
     return this->routes;
 }
 
-// Just another comment....
 Schedule Center::getSchedule() const {
     return this->timetable;
 }
 
-// Will return an QVector
 QVector<TransType> Center::getTransportTypes() const {
     return this->transport_types;
 }

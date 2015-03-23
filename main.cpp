1e36689e530233986e8a2e196bc5424a1ba31a9d
Marius Marusanici <marius.marusanici@gmail.com>
2015-03-23 19:44:48 +0200

    playing with new data types

--- main.cpp
+++ main.cpp
@@ -7,13 +7,18 @@
 #include "client.h"
 #include "position.h"
 #include <QApplication>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
 
-    Center center;
+    Center center = Center("Timisoara", Route("Timisoara", "Arad"), QVector<TransType>({TransType::fast, TransType::slow, TransType::medium}), Schedule());
+    QString foo = "\nDestination is: " + center.getRoute().getDestination() + "\n";
+
+    std::cout << foo.toStdString();
+
     Route route;
     Schedule schedule;
     Driver driver;

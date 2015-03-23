ff294fbedcdd7177c1ef8bba93b1b3372c9c1e7d
Marius Marusanici <marius.marusanici@gmail.com>
2015-03-23 18:39:27 +0200

    constructors for Route class

--- route.h
+++ route.h
@@ -1,15 +1,18 @@
-#include <QString>
-
 #ifndef ROUTE_H
 #define ROUTE_H
 
+#include <QString>
+
 class Route {
+public:
+    Route(): source(QString()), destination(QString()) {}
+    Route(QString& source, QString destination): source(source), destination(destination) {}
+    ~Route() {}
+    QString getSource() const;
+    QString getDestination() const;
 private:
     QString source;
     QString destination;
-public:
-    QString getSource() const;
-    QString getDestination() const;
 };
 
 #endif // ROUTE_H

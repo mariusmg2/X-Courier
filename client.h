5f9c0b348a4bbaec2fa7b42bd9d761c1b887976d
Marius Marusanici <marius.marusanici@gmail.com>
2015-03-23 14:13:55 +0200

    defined Client class in client.h

---
+++ client.h
@@ -0,0 +1,21 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include "person.h"
+
+class Client: public Person {
+public:
+    Client(): Person(), package(Package()), phone(QString()) {}
+    Client(QString &name, bool gender, QDate &born_date, Package& package, QString& phone):
+        Person(name, gender, born_date), package(package), phone(phone) {}
+    ~Client() {}
+    Package getPackage() const;
+    unsigned int getAge() const;
+    virtual QString getContact() const;
+private:
+    Package package;
+    QString phone;
+};
+
+#endif // CLIENT_H
+

85eb46615e07096f9d3eafc5657a8e1d58449458
Marius Marusanici <marius.marusanici@gmail.com>
2015-03-23 14:24:57 +0200

    defined Package class in package.h

---
+++ package.h
@@ -0,0 +1,24 @@
+#ifndef PACKAGE_H
+#define PACKAGE_H
+
+#include <QString>
+#include "pkgtype.h"
+
+class Package {
+public:
+    Package(): weight(0), code(0), status(QString()), type() {}
+    Package(unsigned int weight, unsigned int code, QString& status, PkgType& type):
+        weight(weight), code(code), status(status), type(type) {}
+    ~Package() {}
+    unsigned int getWeight() const;
+    unsigned int getCode() const;
+    QString getStatus() const;
+private:
+    unsigned int weight;
+    unsigned int code;
+    QString status;
+    PkgType type;
+};
+
+#endif // PACKAGE_H
+

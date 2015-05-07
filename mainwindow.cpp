1492662725351f4af2d48821811ea4cd3475bb4f
Marius Marusanici <marius.marusanici@gmail.com>
2015-05-07 16:04:09 +0300

    added new status label in main window, fixed package type selection

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -29,6 +29,8 @@ MainWindow::MainWindow(QWidget *parent) :
     for(auto i : temp) {
         ui->client_destination_combo->addItem(i);
     }
+
+    ui->status_2->setText("Welcome to X-Courier.\nWow, such bullshit, many thanks...");
 }
 
 MainWindow::~MainWindow() {
@@ -49,7 +51,7 @@ bool MainWindow::allDataValid() const {
             || !phoneREX.exactMatch(ui->recipient_phone_line->text()) || ui->recipient_phone_line->text().count() < 8
             || ui->client_phone_line->text().count() < 8 || !mailREX.exactMatch(ui->client_email_line->text())
             || !mailREX.exactMatch(ui->recipient_email_line->text()) || ui->package_name_line->text().isEmpty()
-            || !phoneREX.exactMatch(ui->package_weight_line->text())) {
+            || !phoneREX.exactMatch(ui->package_weight_line->text()) || !phoneREX.exactMatch(ui->package_value_line->text())) {
         return false;
     }
     return true;
@@ -63,10 +65,11 @@ void MainWindow::on_status_clicked() {
 }
 
 void MainWindow::on_send_clicked() {
-    if(!confirmation_ui && this->allDataValid()) {
-        confirmation_ui = QSharedPointer<ConfirmationWindow>(new ConfirmationWindow);
-    }
     if(this->allDataValid()) {
+        if(!confirmation_ui) {
+            confirmation_ui = QSharedPointer<ConfirmationWindow>(new ConfirmationWindow);
+        }
+
         Client client(ui->client_fname_line->text(), ui->client_lname_line->text(),
                       ui->client_phone_line->text(), ui->client_email_line->text());
 
@@ -75,14 +78,30 @@ void MainWindow::on_send_clicked() {
 
         Route route("Timisoara", ui->client_destination_combo->currentText(),
                     database->getShortestRouteDistance("Timisoara", ui->client_destination_combo->currentText()));
+        PkgType aux;
+        QString temp = ui->typeComboBox->currentText();
+
+        if(temp == "Fragile") {
+            aux = PkgType::fragile;
+        }
+        else if(temp == "Precious") {
+            aux = PkgType::precious;
+        }
+        else {
+            aux = PkgType::dangerous;
+        }
 
         Package package(ui->package_weight_line->text().toUInt(), database->getUniqueShippingID(),
-                        10, StatusType::inTransit, ui->package_name_line->text(),
-                        PkgType::precious); //TODO Get package type from UI.
+                        ui->package_value_line->text().toUInt(), StatusType::inTransit, ui->package_name_line->text(), aux);
 
         client.setPackage(package);
         confirmation_ui->setData(client, recipient, route);
 
+        ui->status_2->setText("Wow, such valid data, so small price...");
+
         confirmation_ui->show();
+    }
+    else {
+        ui->status_2->setText("Wow, many mistakes, such invalid\ndata...");
     }
 }
